0b03d64df25bec50515b85e5dc766488b3e46a41
Stckrz <rin449@gmail.com>
2023-10-12 17:30:38 -0500

    we really be fuckin around out here

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import './App.css';
+import { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   const age = 17;
-  const isGreen = false;
   return (
 
-
-    <div className="wtf">
-      <Basicexample />
-
-      <div className="App">
-
-        <header className='App-header'>
-          <div class='alert alert-primary' role='alert'>
-            <p style={{ display: "none" }} className='d-block'>
-              Bootstrap is now successfully installed 😃
-            </p>
-            <p className='d-none'>
-              Bootstrap is not installed if you can see this 😢
-            </p>
-          </div>
-        </header>
-
-
-
-        <div className="content-wrapper">
-          <User name="Stckrz" age={32} email="Stckrz@stckrz.com" />
-          <Job salary="90000" position="Senior SDE" company="Amazon" />
-        </div>
-
-
-        <div className="style-stuff">
-          <ColorPlay />
-        </div>
-        <h1 style={{ color: "red", backgroundColor: "black" }}> THIS HAS COLOR</h1>
-
-      </div>
+    <div className="App">
+      <ShowComponent />
+      <StatePlay />
     </div>
   );
 }
@@ -69,52 +45,93 @@ const ColorPlay = (props) => {
   )
 };
 
+function ShowComponent() {
+  const [isShown, setIsShown] = useState(false);
 
-
-
-
-const Basicexample = () => {
   return (
+    <div>
+      <button onClick={() => setIsShown(!isShown)}>Click</button>
+      {isShown && (
+          <BasicExample />
+      )}
+    </div>
+  )
+}
 
 
-    <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-        <form className="d-flex">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+
+function StatePlay() {
+  const [addValue, setAddValue] = useState(1);
+  const [multiplyValue, setMultiplyValue] = useState(1);
+
+  function doStuff() {
+    setAddValue(addValue + 1);
+    setMultiplyValue(multiplyValue * 2);
+  }
+
+  return (
+    <div className="content-wrapper">
+      <div className="content">
+        <h1>Add one: ({addValue})! <br /> Multiply by two:({multiplyValue})!</h1>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => doStuff()}
+        >Increment</button>
+      </div>
+    </div >
+  )
+}
+
+
+
+
+
+
+
+
+
+
+function BasicExample() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
       </div>
     </nav>
-  )
-
-
+  );
 }
 
 
